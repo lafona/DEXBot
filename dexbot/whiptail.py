119ec4f11090b092d507aa048b86605ccbf7aa62
Ian Haywood <ian@haywood.id.au>
2018-06-03 22:09:15 +1000

    fix view_text

--- dexbot/whiptail.py
+++ dexbot/whiptail.py
@@ -1,12 +1,14 @@
 from __future__ import print_function
 import sys
+import os
 import shlex
 import shutil
 import itertools
-import click
 from subprocess import Popen, PIPE
 from collections import namedtuple
+import tempfile
 
+import click
 
 # whiptail.py - Use whiptail to display dialog boxes from shell scripts
 # Copyright (C) 2013 Marwan Alsabbagh
@@ -85,12 +87,13 @@ class Whiptail:
 
     def view_text(self, text):
         """Whiptail wants a file but we want to provide a text string"""
-        fd, nam = tempfile.mkstemp()
-        f = os.fdopen(fd)
-        f.write(text)
-        f.close()
-        self.view_file(nam)
-        os.unlink(nam)
+        try:
+            fd, nam = tempfile.mkstemp()
+            with os.fdopen(fd, "w") as f:
+                f.write(text)
+            self.view_file(nam)
+        finally:
+            os.unlink(nam)
 
     def clear(self):
         # tidy up the screen
